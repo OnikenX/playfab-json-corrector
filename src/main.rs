3d771fb9af8939f2940588a1277ed69fe188d23e
OnikenX <onikenx@protonmail.com>
2021-10-19 15:25:56 +0100

    Done

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use std::{env::args, fs::read_to_string, io::Write, path::Path};
+
+fn main() {
+    let args : Vec<_> = args().collect();
+    if args.len() != 3 {
+        eprintln!("program <input.json> <output.json>");
+        return;
+    }
+    let mut string = read_to_string(args.get(1).expect("No 1st argument")).expect("Cann't read string.");
+    string = string.replace("\"{", "{").replace("}\"", "}").replace("\\r\\n", "").replace("\\\"", "\"");
+    let mut file = std::fs::File::create(args.get(2).expect("No 2nd argument")).expect("create failed");
+    file.write_all(string.as_bytes()).expect("write failed");
+}

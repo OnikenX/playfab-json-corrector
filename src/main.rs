37af6a309ea28c125e6632901fe9c0883486ac16
OnikenX <onikenx@protonmail.com>
2021-10-19 15:36:20 +0100

    changed to work on stable and adde \n just in case

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env::args, fs::read_to_string, io::Write, path::Path};
+use std::{env::args, fs::read_to_string, io::Write};
 
 fn main() {
     let args : Vec<_> = args().collect();
@@ -7,7 +7,7 @@ fn main() {
         return;
     }
     let mut string = read_to_string(args.get(1).expect("No 1st argument")).expect("Cann't read string.");
-    string = string.replace("\"{", "{").replace("}\"", "}").replace("\\r\\n", "").replace("\\\"", "\"");
+    string = string.replace("\"{", "{").replace("}\"", "}").replace("\\r\\n", "").replace("\\n", "").replace("\\\"", "\"");
     let mut file = std::fs::File::create(args.get(2).expect("No 2nd argument")).expect("create failed");
     file.write_all(string.as_bytes()).expect("write failed");
 }
